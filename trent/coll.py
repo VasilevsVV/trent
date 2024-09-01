cd75860f8c8659bb37c59a94026f8a59329c005e
Vasyl Tsiura <v.tsiura@medzakupivli.com>
2024-09-01 21:03:02 +0300

    Added methods concat, extend, append, extebnd_, append_ to coll.

--- trent/coll.py
+++ trent/coll.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
+
+import itertools as it
 from functools import reduce
 from itertools import chain
 from pprint import pprint
-from typing import Callable, Iterable, Iterator, Optional, TypeVar, overload
+from typing import Any, Callable, Iterable, Iterator, Optional, TypeVar, overload
 
 T = TypeVar('T')
 T1 = TypeVar('T1')
@@ -38,6 +40,46 @@ class coll(Iterable[T]):
         m = map(f, self.__coll)
         m = reduce(chain, m)
         return coll(m)
+    
+    
+    def concat(self, *__iterables: Iterable[T]) -> coll[T]:
+        res = coll()
+        for __it in __iterables:
+            res.extend_(__it)
+        return res
+    
+    def extend(self, __iterable: Iterable[T]) -> coll[T]:
+        return self.concat(__iterable)
+    
+    
+    def append(self, __val: T) -> coll[T]:
+        return coll(self.__coll).append_(__val)
+
+    
+    def __add__(self, __iter: Iterable[T]) -> coll[T]:
+        return self.concat(__iter)
+    
+    # ===============================================================
+    #   IN_PLACE TRANSFORMATIONS
+    
+    def extend_(self, __iterable: Iterable[T]) -> coll[T]:
+        """In-place extend. Addes `__iterable` to the end of `coll`.
+
+        Args:
+            __iterable (Iterable[T]): Iterable to be concatenated to the end
+
+        Returns:
+            coll[T]: Self
+        """        
+        if isinstance(__iterable, coll):
+            self.__coll = chain(self.__coll, __iterable.collection)
+        self.__coll = chain(self.__coll, __iterable)
+        return self
+    
+    def append_(self, __val: T) -> coll[T]:
+        self.extend_([__val])
+        return self
+    
     
     
     @overload
@@ -81,5 +123,6 @@ class coll(Iterable[T]):
 if __name__ == '__main__':
     def s_add(s1: str, s2: str):
         return s1 + s2
-    c = coll([1,2,3,4]).map(lambda x: x+1).mapcat(lambda x: range(x)).map(str).reduce(s_add)
+    c = coll([1,2,3,4])
+    c = c.concat(b'56')
     pprint(c)
