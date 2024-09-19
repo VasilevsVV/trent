b482ceb7a860a2fcb76db2252adea761b9ab133c
Vasyl tsiura <v.tsiura@medzakupivli.com>
2024-09-19 14:48:02 +0300

    Added append, append_, extend, extend_, pmap to coll

--- trent/coll.py
+++ trent/coll.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
-from functools import reduce
+import concurrent.futures as conc
+from functools import reduce, cache
+from funcy import filter
 from itertools import chain
 from pprint import pprint
+from time import sleep
 from typing import Any, Callable, Iterable, Iterator, Optional, TypeVar, overload
 
 T = TypeVar('T')
@@ -10,12 +13,39 @@ T1 = TypeVar('T1')
 S = TypeVar('S')
 
 
+def __cpu_count():
+    try:
+        import psutil
+        return psutil.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+    
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+    return 8
+
+
+DEFAULT_THREAD_COUNT = __cpu_count()
+
+
+def __make_threadpool(threads: int = DEFAULT_THREAD_COUNT):
+    return conc.ThreadPoolExecutor(threads)
+
+
+TRENT_THREADPOOL = conc.ThreadPoolExecutor(DEFAULT_THREAD_COUNT, 'trent')
+    
+
+
+
 class NestedIterationExceprion(Exception):
     def __repr__(self) -> str:
         return 'Nested iteration over `coll` class is invalid !!!'
 
 
-class coll(Iterable[T]):
+class icoll(Iterable[T]):
     def __init__(self, collection: Optional[Iterable[T]] = None) -> None:
         self.__coll: Iterable[T]
         self.__buffer: list[T]
@@ -30,37 +60,58 @@ class coll(Iterable[T]):
     def collection(self) -> Iterable[T]:
         return self.__coll
     
-    def map(self, f: Callable[[T], T1]) -> coll[T1]:
-        return coll(map(f, self.__coll))
+    
+    # ==================================================================
+    #           MAPS
+    
+    def map(self, f: Callable[[T], T1]) -> icoll[T1]:
+        return icoll(map(f, self.__coll))
     
     
-    def mapcat(self, f: Callable[[T], Iterable[T1]]) -> coll[T1]:
+    def pmap(self, f: Callable[[T], S]) -> icoll[S]:
+        __map = TRENT_THREADPOOL.map(f, self.__coll)
+        return icoll(__map)
+    
+    
+    def pmap_(self, f, threads=DEFAULT_THREAD_COUNT):
+        assert threads >= 1, 'Async Thread count CAN NOT be < 1'
+        if threads == 1:
+            return self.map(f)
+        with conc.ThreadPoolExecutor(threads) as p:
+            __map = p.map(f, self.__coll)
+        return icoll(__map)
+    
+    
+    def mapcat(self, f: Callable[[T], Iterable[T1]]) -> icoll[T1]:
         m = map(f, self.__coll)
         m = reduce(chain, m)
-        return coll(m)
+        return icoll(m)
     
     
-    def concat(self, *__iterables: Iterable[T]) -> coll[T]:
-        res = coll(self.__coll)
+    # ==================================================================
+    #           TRANSFORMATIONS
+    
+    def concat(self, *__iterables: Iterable[T]) -> icoll[T]:
+        res = icoll(self.__coll)
         for __it in __iterables:
             res.extend_(__it)
         return res
     
-    def extend(self, __iterable: Iterable[T]) -> coll[T]:
+    def extend(self, __iterable: Iterable[T]) -> icoll[T]:
         return self.concat(__iterable)
     
     
-    def append(self, __val: T) -> coll[T]:
-        return coll(self.__coll).append_(__val)
+    def append(self, __val: T) -> icoll[T]:
+        return icoll(self.__coll).append_(__val)
 
     
-    def __add__(self, __iter: Iterable[T]) -> coll[T]:
+    def __add__(self, __iter: Iterable[T]) -> icoll[T]:
         return self.concat(__iter)
     
     # ===============================================================
     #   IN_PLACE TRANSFORMATIONS
     
-    def extend_(self, __iterable: Iterable[T]) -> coll[T]:
+    def extend_(self, __iterable: Iterable[T]) -> icoll[T]:
         """In-place extend. Addes `__iterable` to the end of `coll`.
 
         Args:
@@ -69,17 +120,19 @@ class coll(Iterable[T]):
         Returns:
             coll[T]: Self
         """        
-        if isinstance(__iterable, coll):
+        if isinstance(__iterable, icoll):
             self.__coll = chain(self.__coll, __iterable.collection)
             return self
         self.__coll = chain(self.__coll, __iterable)
         return self
     
-    def append_(self, __val: T) -> coll[T]:
+    def append_(self, __val: T) -> icoll[T]:
         self.extend_([__val])
         return self
     
     
+    # =================================================================
+    #           COLLECTING
     
     @overload
     def reduce(self, f: Callable[[T, T], T]) -> T: ...
@@ -117,16 +170,30 @@ class coll(Iterable[T]):
         return f'coll({lst})'
 
 
-
+class coll(icoll, Iterable[T]):
+    def __init__(self, collection: Optional[Iterable[T]] = None) -> None:
+        super().__init__(collection)
+    
+    
+    def __iter__(self):
+        if self.__is_iterated:
+            raise NestedIterationExceprion
+        self.__buffer = []
+        self.__iter = iter(self.__coll)
+        self.__is_iterated = True
+        return self
+    
+    
+    def __next__(self) -> T:
+        try:
+            val = next(self.__iter)
+        except StopIteration:
+            self.__coll = self.__buffer
+            self.__is_iterated = False
+            raise StopIteration
+        self.__buffer.append(val)
+        return val
 
 
 if __name__ == '__main__':
-    def s_add(s1: str, s2: str):
-        return s1 + s2
-    c = coll([1,2,3,4])
-    c = c.concat(b'56')
-    
-    def add(a: str, b: int):
-        return a + str(b)
-    pprint(c.reduce(add, ''))
-    print(c)
+    pass
