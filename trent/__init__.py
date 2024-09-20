40559fe68872b1809e933abe6fc0eb61593baa6d
Vasyl tsiura <v.tsiura@medzakupivli.com>
2024-09-20 17:44:16 +0300

    Fixed __init__

--- trent/__init__.py
+++ trent/__init__.py
@@ -1,2 +1,2 @@
 from .coll import icoll
-from interface import coll, cmap, cfilter, pmap, pmap_, cat, mapcat, catmap, pairmap, groupmap, map_to_pair
+from .interface import coll, cmap, cfilter, pmap, pmap_, cat, mapcat, catmap, pairmap, groupmap, map_to_pair
