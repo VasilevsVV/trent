c56d153ea62eea1aa0ac1b8a2957c181cd63f798
Vasyl tsiura <v.tsiura@medzakupivli.com>
2024-09-20 12:47:56 +0300

    Added a bunch of utiliti functions (first, second, nth, first_, seond_ etc.)

--- trent/func.py
+++ trent/func.py
@@ -1,9 +1,61 @@
-from typing import Any, TypeVar
+from typing import Any, Iterable, Optional, Sequence, TypeVar
 
 
+class MissingValueException(Exception):
+    def __init__(self, val, fn_name: str) -> None:
+        self._value = val
+        self._fn_name = fn_name
+    
+    def __repr__(self) -> str:
+        return f'Missing {self._fn_name} for value: {self._value}'
 
-T = TypeVar('T')
+# ======================================================
+
+_T = TypeVar('_T')
 
 
-def identity(val: T) -> T:
-    return val 
+def identity(val):
+    return val
+
+
+def _nth(coll:Optional[Iterable[_T]], n:int, position_name) -> Optional[_T]:
+    if (isinstance(coll, Sequence)):
+        if len(coll) > n:
+            return coll[n]
+        return None
+    if isinstance(coll, Iterable):
+        it = iter(coll)
+        i = 0
+        while i < n:
+            next(it)
+            i += 1
+        return next(it)
+    if coll is None:
+        return None
+    raise Exception("Cant get '{}' attribute from value: {}.\n It is not a Collection|None".format(position_name, coll))
+
+def first(coll: Iterable[_T]|Any) -> Optional[_T]:
+    return _nth(coll, 0, 'first')
+
+def second(coll: Iterable[_T]|Any) -> Optional[_T]:
+    return _nth(coll, 1, 'second')
+
+def third(coll: Iterable[_T]|Any) -> Optional[_T]:
+    return _nth(coll, 2, 'third')
+
+def nth(coll: Iterable[_T]|Any, n:int) -> Optional[_T]:
+    return _nth(coll, n, 'nth')
+
+
+def first_(coll: Iterable[_T]) -> _T:
+    res = _nth(coll, 0, 'first_')
+    if res is None:
+        raise MissingValueException(coll, 'first')
+    return res
+
+
+def second_(coll: Iterable[_T]) -> _T:
+    res = _nth(coll, 1, 'second_')
+    if res is None:
+        raise MissingValueException(coll, 'second')
+    return res
