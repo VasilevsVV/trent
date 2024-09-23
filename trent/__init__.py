2b2a00d76177b172e398bc81b8c05dd6817aadb7
Vasyl tsiura <v.tsiura@medzakupivli.com>
2024-09-23 16:04:46 +0300

    Updated __init__

--- trent/__init__.py
+++ trent/__init__.py
@@ -1,2 +1,18 @@
 from .coll import icoll
-from .interface import coll, cmap, cfilter, pmap, pmap_, cat, mapcat, catmap, pairmap, groupmap, map_to_pair
+from .interface import coll, cmap, cfilter, pmap, pmap_, cat, mapcat, catmap, pairmap, groupcoll, groupmap, map_to_pair
+
+
+__all__ = [
+    'coll',
+    'cmap', 
+    'cfilter',
+    'pmap',
+    'pmap_',
+    'cat',
+    'mapcat',
+    'catmap',
+    'pairmap',
+    'groupmap',
+    'groupcoll',
+    'map_to_pair'
+]
