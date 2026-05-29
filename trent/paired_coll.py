ed69c5f6a3b1015217497782eb0752199680704f
Vasyl Tsiura <morfey.rulit@gmail.com>
2026-05-29 22:24:24 +0300

    Reworked pairmap. Added pairmap_to_pair

--- trent/paired_coll.py
+++ trent/paired_coll.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from typing import Any, Callable, Dict, Generic, Iterable, TypeVar, Tuple, overload
 from trent.func import identity
 from trent.nth import first, first_, second, second_
 from trent.collection_base import C, R1, R2, T1, T2, icoll_base
 
+if TYPE_CHECKING:
+    from coll import icoll
+
 
 class paired_icoll(icoll_base[Tuple[T1, T2]], Iterable[Tuple[T1, T2]]):
-    @overload
-    def pairmap(self, f:Callable[[T1, T2], Tuple[R1, R2]]) -> "paired_icoll[R1, R2]": ...
-    @overload
-    def pairmap(self, f:Callable[[T1, T2], R1]) -> icoll_base[R1]: ...
 
-    def pairmap(self, f:Callable[[T1, T2], Any]): # type: ignore
+    
+    def pairmap(self, f:Callable[[T1, T2], R1]) -> icoll[R1]:
         """Map over paired elements (tuple, list, Iterable, etc.) with `f(arg1, arg2)` function.
         WARNING: sequence elements MUST be iterables.
         NOTE: Iterable elements can contain more than 2 elements, but extra values will be lost.
@@ -24,15 +27,11 @@ class paired_icoll(icoll_base[Tuple[T1, T2]], Iterable[Tuple[T1, T2]]):
         """
         def _f(_val: Tuple[T1, T2]):
             return f(first_(_val), second_(_val))
-        if self.empty:
-            return self._step(self).map(_f) # just for consistency.
-        _h = _f(self.head) # type: ignore
-        if (
-            isinstance(_h, Tuple) 
-            and len(_h) == 2
-        ):
-            return paired_icoll(map(_f, self.collection))
         return self._step(self).map(_f)
+    
+
+    def pairmap_to_pair(self, f: Callable[[T1, T2], Tuple[R1, R2]]) -> paired_icoll[R1, R2]:
+        return paired_icoll(self.pairmap(f))
     
 
     def group_by(
