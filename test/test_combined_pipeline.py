b9cabdf70350499075d65b30ad6d8ed3bd961041
Vasyl Tsiura <morfey.rulit@gmail.com>
2026-06-13 11:18:35 +0300

    Added some more tests

--- test/test_combined_pipeline.py
+++ test/test_combined_pipeline.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 
+from trent.func import identity
 from trent.interface import seq, map_to_pair
 from trent.nth import first, first_, second
 
@@ -8,7 +9,7 @@ def test_combined_colls_1():
     c = (
         seq(range(10))
         .map_to_pair(lambda x: x % 3)
-        .pairmap(lambda x,y: (f'group_{x}', y))
+        .pairmap_to_pair(lambda x,y: (f'group_{x}', y))
         .group_by(first_, second)
     )
     res = dict(c)
@@ -19,15 +20,28 @@ def test_combined_colls_1():
     }
 
 
-if __name__ == '__main__':
+def test_combined_colls_2():
     c = (
         seq(range(10))
-        .map_to_pair(lambda x: x % 3)
-        .pairmap(lambda x,y: (f'group_{x}', y))
-        .group_by(first_, second)
-        .persist()
-        # .to_list()
+        .mapcat(lambda x: range(x))
+        .group_by(identity)
+        .pairmap_to_pair(lambda x,lst: (x, len(lst)))
+        .to_dict()
     )
-    pprint(c)
-    # pprint(c.to_list())
-    pprint(dict(c))
+    assert c == {0: 9, 1: 8, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1}
+
+
+def _join(res: str, vals: tuple[int, int]):
+        if res.strip() == '':
+            return f'{vals[0]}: {vals[1]}'
+        return res + f', {vals[0]}: {vals[1]}'
+
+def test_combined_colls_3():
+    c = (
+        seq(range(10))
+        .mapcat(lambda x: range(x))
+        .group_by(identity)
+        .pairmap_to_pair(lambda x,lst: (x, len(lst)))
+        .reduce(_join, '')
+    )
+    assert c == '0: 9, 1: 8, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1'
